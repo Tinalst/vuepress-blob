28a055049071161a9575648df295de66f747d928
tinalim <lst_tina@163.com>
2019-05-21 11:08:11 +0800

    提交测试

---
+++ .vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'title is here',
+  description: 'just playing around',
+  themeConfig:{
+    nav: [
+      {text: 'home', link: '/'},
+      {text: 'readme2', link: '/readme2.md'},
+      {text: 'external', link: 'http://google.com'},
+      {text: 'language', items: [
+          {text: 'chinese', link: '/language/chinese'},
+          {text: 'japanese', link: '/language/japanese'}
+      ]},
+      {text: 'groups', items: [
+          {text: 'group1', items: [
+              {text: 'group1-1', link: '/'},
+              {text: 'group1-2', link: '/'}
+          ]}
+      ]}
+    ],
+    sidebar: [
+      '/readme2',
+      ['/readme3', 'explicit link text']
+    ]
+  }
+};
